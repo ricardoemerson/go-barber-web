a20a405ba041260b37a8e4bc507d2074b12003c6
Andréia Crystina <andreia.crystina@hotmail.com>
2019-12-28 22:55:52 -0300

    Implementado o armazenamento do perfil e a persistência da autenticação com redux-persit.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import './config/ReactotronConfig';
 
@@ -9,15 +10,17 @@ import history from './services/history';
 import GlobalStyle from './styles/global';
 
 import Routes from './routes';
-import store from './store';
+import { store, persistor } from './store';
 
 function App() {
   return (
     <Provider store={ store }>
-      <Router history={ history }>
-        <Routes />
-        <GlobalStyle />
-      </Router>
+      <PersistGate persistor={ persistor }>
+        <Router history={ history }>
+          <Routes />
+          <GlobalStyle />
+        </Router>
+      </PersistGate>
     </Provider>
   );
 }
---
+++ src/store/persistReducers.js
@@ -0,0 +1,12 @@
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+export default reducers => {
+  const persistedReducer = persistReducer({
+    key: 'gobarber',
+    storage,
+    whitelist: ['auth', 'user'],
+  }, reducers);
+
+  return persistedReducer;
+};
