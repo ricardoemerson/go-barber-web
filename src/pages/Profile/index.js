9165f3791166dcb0e2851f73734a3a1bac5654b3
Andréia Crystina <andreia.crystina@hotmail.com>
2019-12-29 20:48:24 -0300

    Implementada a página de atualização do perfil.

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
-// import { Container } from './styles';
+import { Form, Input } from '@rocketseat/unform';
+
+import { Container } from './styles';
+
+import { updateProfileRequest } from '~/store/modules/user/actions';
 
 export default function Profile() {
+  const dispatch = useDispatch();
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
+
   return (
-    <h1>Profile</h1>
+    <Container>
+      <Form initialData={ profile } onSubmit={ handleSubmit }>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" placeholder="Seu endereço de e-mail" />
+
+        <hr />
+
+        <Input name="oldPassword" placeholder="Sua senha atual" />
+        <Input name="password" placeholder="Nova senha" />
+        <Input name="confirmPassword" placeholder="Confirmação de senha" />
+
+        <button type="submit">Atualizar Perfil</button>
+      </Form>
+
+      <button type="button">Sair do GoBarber</button>
+    </Container>
   );
 }
