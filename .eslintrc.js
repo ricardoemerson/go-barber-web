54f0f2318897cf21c2d9677e03e559b42e567df7
Andréia Crystina <andreia.crystina@hotmail.com>
2019-12-30 22:51:01 -0300

    Estilização do dashboard. Implementada a navegação entre os dias e a listagem dos agendamentos.

--- .eslintrc.js
+++ .eslintrc.js
@@ -58,6 +58,7 @@ module.exports = {
               '/^@rocketseat/',
               '/^react-icons/',
               '/^react-native-vector-icons/',
+              '/^date-fns/',
               '/services/',
               '/pages/',
               '/components/',
--- src/components/Notifications/index.js
+++ src/components/Notifications/index.js
@@ -1,8 +1,9 @@
-import { parseISO, formatDistance } from 'date-fns';
-import pt from 'date-fns/locale/pt';
 import React, { useState, useEffect, useMemo } from 'react';
 
 import { MdNotifications } from 'react-icons/md';
+
+import { parseISO, formatDistance } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 
 import api from '~/services/api';
 
